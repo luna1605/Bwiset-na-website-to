0fd08ca20b4e8e9dc75e63888091e6a052b7332e
luna1605 <maryannefernandez16@gmail.com>
2024-11-06 00:31:38 +0800

    Delete webdev_midterms-main directory

--- webdev_midterms-main/PrelimWEBSITE-main/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // Unset all session variables
-session_destroy(); // Destroy the session
-
-// Redirect to login page with a message
-header("Location: accadmin.php");
-exit();
-?>
